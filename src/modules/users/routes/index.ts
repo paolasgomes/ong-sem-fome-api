04ec89835dea9da995ad05fdbd873a0d0e18b0c0
Paola Gomes <paola@frengee.com>
2025-11-19 16:05:07 -0300

    feat: get all users

--- src/modules/users/routes/index.ts
+++ src/modules/users/routes/index.ts
@@ -2,6 +2,7 @@ import { verifyToken } from '@/modules/auth/middlewares';
 import { Router } from 'express';
 import { createUser } from '../controllers/create';
 import { updateUser } from '../controllers/update';
+import { getAllUsers } from '../controllers/getAll';
 
 const usersRoutes = Router();
 
@@ -9,5 +10,6 @@ usersRoutes.use(verifyToken);
 
 usersRoutes.post('/', createUser);
 usersRoutes.put('/:id', updateUser);
+usersRoutes.get('/', getAllUsers);
 
 export { usersRoutes };
