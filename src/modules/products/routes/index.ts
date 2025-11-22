bc33d1cfd2d5d40923dff6383ea6c8a3f5d03c0b
Paola Gomes <paola@frengee.com>
2025-11-21 22:13:02 -0300

    feat: add update product

--- src/modules/products/routes/index.ts
+++ src/modules/products/routes/index.ts
@@ -2,6 +2,7 @@ import { verifyToken } from '@/modules/auth/middlewares';
 import { Router } from 'express';
 import { createProduct } from '../controllers/create';
 import { getAllProducts } from '../controllers/getAll';
+import { updateProduct } from '../controllers/update';
 
 const productsRoutes = Router();
 
@@ -9,5 +10,6 @@ productsRoutes.use(verifyToken);
 
 productsRoutes.post('/', createProduct);
 productsRoutes.get('/', getAllProducts);
+productsRoutes.put('/:id', updateProduct);
 
 export { productsRoutes };
