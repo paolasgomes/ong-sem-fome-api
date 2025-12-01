ac85c5dfa6d9b6e1cdae9b33de3bc439d5f92147
Paola Gomes <paola@frengee.com>
2025-12-01 10:36:08 -0300

    feat: get all campaigns

--- src/modules/campaigns/routes/index.ts
+++ src/modules/campaigns/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { createCampaign } from '../controllers/create';
 import { updateCampaign } from '../controllers/update';
+import { getAllCampaigns } from '../controllers/getAll';
 
 const campaignsRoutes = Router();
 
 campaignsRoutes.post('/', createCampaign);
 campaignsRoutes.put('/:id', updateCampaign);
+campaignsRoutes.get('/', getAllCampaigns);
 
 export { campaignsRoutes };
