6693ebb3a5cf4612e2d0867df08e4ef0ecd88ce9
Paola Gomes <paola@frengee.com>
2025-12-10 14:23:08 -0300

    refactor: api

--- src/api/index.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import serverless from 'serverless-http';
-import app from '../app';
-
-const handler = serverless(app);
-
-export default handler;
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './config/swagger';
 import { routes } from './routes';
+import api from '../api';
 
 const app = express();
 
@@ -11,5 +12,7 @@ app.use(express.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(routes);
+
+app.use('/api', api);
 
 export default app;
