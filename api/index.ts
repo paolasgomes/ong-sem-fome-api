b207512e71425927cae1b831d0c61f288becab55
Paola Gomes <paola@frengee.com>
2025-12-10 15:24:08 -0300

    eqwe

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,11 @@
+import moduleAlias from 'module-alias';
+import path from 'path';
+
+// Registrar aliases para funcionar na Vercel
+moduleAlias.addAliases({
+  '@': path.join(__dirname, '..', 'src'),
+});
+
 import 'dotenv/config';
 import app from '../src/app';
 
