11d51700b88832b975b25bff440c3b203e1072c5
Paola Gomes <paola@frengee.com>
2025-12-10 14:28:17 -0300

    remove dist

--- dist/src/modules/auth/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRoutes = void 0;
-const express_1 = require("express");
-const auth_1 = require("../controllers/auth");
-const authRoutes = (0, express_1.Router)();
-exports.authRoutes = authRoutes;
-authRoutes.post('/', auth_1.login);
--- dist/src/modules/donations/enums/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ALLOWED_UNITS = exports.ALLOWED_TYPES = void 0;
-const ALLOWED_TYPES = ['food', 'clothing', 'money', 'campaign'];
-exports.ALLOWED_TYPES = ALLOWED_TYPES;
-const ALLOWED_UNITS = ['kg', 'g', 'l', 'ml', 'un'];
-exports.ALLOWED_UNITS = ALLOWED_UNITS;
